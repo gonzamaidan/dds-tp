cc7ef6eff88c24577855e851659af05ca4dcec7d
Iña <isauri.github2@goat.si>
2018-04-21 14:40:08 -0300

    Se crea Administrador. Basado en el del branch refactor-clientes.

---
+++ src/main/java/dominio/administradores/Administrador.java
@@ -0,0 +1,27 @@
+package dominio.administradores;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Administrador {
+
+	private Integer idSistema;
+	private String nombreYApellido;
+	private String domicilio;
+	private LocalDate fechaDeAlta;
+
+	public Administrador(String nombreYApellido, String domicilio, LocalDate fechaDeAlta, Integer idSistema) {
+		this.nombreYApellido = nombreYApellido;
+		this.domicilio = domicilio;
+		this.fechaDeAlta = fechaDeAlta;
+		this.idSistema = idSistema;
+	}
+
+	public LocalDate getFechaDeAlta() {
+		return fechaDeAlta;
+	}
+
+	public Long mesesComoAdministrador() {
+		return ChronoUnit.MONTHS.between(this.fechaDeAlta, LocalDate.now());
+	}
+}
