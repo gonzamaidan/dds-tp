1b9c694aa7eb292b561865831061e0e4a477833b
iña <isauri.github2@goat.si>
2018-04-26 16:44:13 -0300

    Agrego comportamiento para levantar Clientes del Json

--- src/main/java/funcional/RepositorioUsuarios.java
+++ src/main/java/funcional/RepositorioUsuarios.java
@@ -1,5 +1,58 @@
 package funcional;
 
-public class RepositorioUsuarios {
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.List;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import dominio.clientes.Cliente;
+
+public class RepositorioUsuarios {
+	
+	private List<Cliente> clientes;
+	private static RepositorioUsuarios repositorio = null;
+
+	protected RepositorioUsuarios() {
+		// para evitar otras instanciaciones
+	}
+	
+	public RepositorioUsuarios getRepositorio() {
+		if (repositorio == null) {
+			repositorio = new RepositorioUsuarios();
+		}
+		return repositorio;
+	}
+	
+	public void load(Path archivoClientes) throws FileNotFoundException {
+		Gson gson = new GsonBuilder().
+				registerTypeAdapter(LocalDate.class, new JsonDeserializer<LocalDate>() {
+					@Override
+					public LocalDate deserialize(JsonElement json, Type type,
+					JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+						String date = json.getAsString();
+						return LocalDate.parse(date);
+					}
+				})
+				.create();
+
+		JsonReader reader = new JsonReader(new FileReader(archivoClientes.toFile()));
+		Type listType = new TypeToken<List<Cliente>>() {}.getType();
+
+		this.clientes = gson.fromJson(reader, listType);
+	}
+
+	public List<Cliente> getClientes() {
+		return this.clientes;
+	}
 }
