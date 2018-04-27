3e55a3c4868e74aa850c9deb36d45da737bb5d33
iña <isauri.github2@goat.si>
2018-04-26 21:03:01 -0300

    Agrego tests Reposotorio. Arreglar uso de path hardcodeado...

---
+++ src/test/java/funcional/RepositorioUsuariosTest.java
@@ -0,0 +1,72 @@
+package funcional;
+
+import funcional.RepositorioUsuarios;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gson.JsonSyntaxException;
+
+public class RepositorioUsuariosTest {
+
+	Path pathClientes;
+	Path pathClientesDefectuoso;
+	RepositorioUsuarios repo;
+	
+	@Before
+	public void TestSetup() {
+		pathClientes = Paths.get("/home/lain/eclipse-workspace/2018-vn-group-17/src/test/resources/archivoClientes.json");
+		pathClientesDefectuoso = Paths.get("/home/lain/eclipse-workspace/2018-vn-group-17/src/test/resources/archivoClientesDefectuoso.json");
+		repo = RepositorioUsuarios.getRepositorio();
+	}
+	
+	@Test
+	public void elRepositorioPuedeLevantarElArchivoDeClientes() throws FileNotFoundException {
+		repo.cargarClientes(pathClientes);
+		assertNotNull(repo.getClientes());
+	}
+
+	@Test
+	public void elRepositorioPuedeLevantarArchivosDeClientesVacios() throws FileNotFoundException, IOException{
+		File dummyFile = new File("clientesDummy.json");
+		dummyFile.createNewFile();
+		Path dummyJson = Paths.get(dummyFile.getAbsolutePath());
+
+		repo.cargarClientes(dummyJson);
+		dummyFile.delete();
+	}
+
+	// TODO: este test estaria muy acoplado al archivo del dominio, que va a cambiar mucho...
+	@Test
+	public void cargarUnArchivoConTresClientesTraeLosTres() throws FileNotFoundException{
+		int cantidadEsperada = 3;
+		repo.cargarClientes(pathClientes);
+		
+		assertEquals(cantidadEsperada, repo.getClientes().size());
+	}
+	
+	@Test
+	public void cargarElRepositorioMasDeUnaVezNoExtiendeLaListaDeClientes() throws FileNotFoundException {
+		repo.cargarClientes(pathClientes);
+		int sizeInicial = repo.getClientes().size();
+		
+		repo.cargarClientes(pathClientes);
+		
+		assertEquals(sizeInicial, repo.getClientes().size());
+	}
+	
+	// TODO: creo que habria que ver que onda con que casos de error prevenir
+	@Test(expected = JsonSyntaxException.class)
+	public void cargarUnArchivoDefectuosoProduceUnaFalla() throws FileNotFoundException{
+		repo.cargarClientes(pathClientesDefectuoso);
+	}	
+}
