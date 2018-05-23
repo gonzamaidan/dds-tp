1c38a416ae41cfa9610f3746e1f802a15fb3b71b
iña <isauri.github2@goat.si>
2018-05-23 19:53:51 -0300

    Cambio constructor Serializador y metodo ya no toma un archivo por parametro.

--- src/main/java/funcional/InterpreteAlmacenamiento.java
+++ src/main/java/funcional/InterpreteAlmacenamiento.java
@@ -7,6 +7,6 @@ import dominio.excepciones.FalloTraduccionException;
 
 public interface InterpreteAlmacenamiento {
 
-	public List<Cliente> traducirAClientes(String formaAlmacenamiento) throws FalloTraduccionException;
+	public List<Cliente> traducirAClientes() throws FalloTraduccionException;
 	abstract void validarFormaAlmacenamiento(String formaAlmacenamiento) throws FalloTraduccionException;
 }
--- src/main/java/funcional/SerializadorJson.java
+++ src/main/java/funcional/SerializadorJson.java
@@ -26,16 +26,16 @@ public class SerializadorJson implements InterpreteAlmacenamiento {
 	private Gson deserializador;
 	private Path archivoClientes;
 
-	public SerializadorJson() {
+	public SerializadorJson(String formaAlmacenamiento) throws FalloTraduccionException {
 		this.deserializador = new GsonBuilder()
 								.registerTypeAdapter(LocalDate.class, this.localDateDeserializer())
 								.create();
+		this.validarFormaAlmacenamiento(formaAlmacenamiento);
 	}
 
-	public List<Cliente> traducirAClientes(String formaAlmacenamiento) throws FalloTraduccionException {
-		this.validarFormaAlmacenamiento(formaAlmacenamiento);
+	public List<Cliente> traducirAClientes() throws FalloTraduccionException {
 		try {
-			return this.deserializarClientes(archivoClientes);
+			return this.deserializarClientes();
 		} catch (FileNotFoundException e) {
 			throw new FalloTraduccionException("No se pudo traducir la forma de almacenamiento", e);
 		}
@@ -46,11 +46,11 @@ public class SerializadorJson implements InterpreteAlmacenamiento {
 			this.archivoClientes = Paths.get(formaAlmacenamiento);
 		} catch (InvalidPathException e) {
 			throw new FalloTraduccionException(
-					"La forma de almacenamiento: " + formaAlmacenamiento + " no es un directorio valido", e);
+					"La forma de almacenamiento: " + formaAlmacenamiento + " no es un archivo Json valido", e);
 		}
 	}
 
-	public List<Cliente> deserializarClientes(Path archivoClientes) throws FileNotFoundException {
+	public List<Cliente> deserializarClientes() throws FileNotFoundException {
 		JsonReader jsonReader = new JsonReader(new FileReader(archivoClientes.toFile()));
 		Type tipoLista = new TypeToken<List<Cliente>>() {
 		}.getType();
