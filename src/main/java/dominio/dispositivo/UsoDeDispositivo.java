abae6e4e0a1d30843e1e77d2396d588f4820737d
Taiel Deluermoz <tdeluermoz@est.frba.utn.edu.ar>
2018-05-26 01:25:59 -0300

    corrijo el between del horasDeUsoEntre porque sino da negativo

--- src/main/java/dominio/dispositivo/UsoDeDispositivo.java
+++ src/main/java/dominio/dispositivo/UsoDeDispositivo.java
@@ -1,40 +1,39 @@
 package dominio.dispositivo;
+
 import java.time.LocalDateTime;
 
 import java.time.temporal.ChronoUnit;
-
 
 public class UsoDeDispositivo {
 
 	LocalDateTime fechaHoraEncendido;
 	LocalDateTime fechaHoraApagado;
-	
+
 	/*
-	public double cantidadDehorasUsado() {
-		return ChronoUnit.HOURS.between(fechaHoraEncendido, fechaHoraApagado);
-	}*/
-	
+	 * public double cantidadDehorasUsado() { return
+	 * ChronoUnit.HOURS.between(fechaHoraEncendido, fechaHoraApagado); }
+	 */
+
 	public double horasDeUsoEntre(LocalDateTime fechaInicio, LocalDateTime fechaFin) {
 		LocalDateTime fechaMenor = this.maximoEntre(fechaInicio, fechaHoraEncendido);
 		LocalDateTime fechaMayor = this.minimoEntre(fechaFin, fechaHoraApagado);
-				
-				
-		return ChronoUnit.HOURS.between(fechaMayor, fechaMenor);
-	
+
+		return ChronoUnit.HOURS.between(fechaMenor, fechaMayor);
+
 	}
-	
+
 	public LocalDateTime maximoEntre(LocalDateTime fecha1, LocalDateTime fecha2) {
 		if (fecha1.isAfter(fecha2)) {
 			return fecha1;
-		}else {
+		} else {
 			return fecha2;
 		}
 	}
-	
+
 	public LocalDateTime minimoEntre(LocalDateTime fecha1, LocalDateTime fecha2) {
 		if (fecha1.isBefore(fecha2)) {
 			return fecha1;
-		}else {
+		} else {
 			return fecha2;
 		}
 	}
@@ -46,5 +45,5 @@ public class UsoDeDispositivo {
 	public void setFechaHoraApagado(LocalDateTime fechaHoraApagado) {
 		this.fechaHoraApagado = fechaHoraApagado;
 	}
-	
+
 }
