38a93ba94a5d8aa2b7380fcbc05b93ec0619b9bd
iña <isauri.github2@goat.si>
2018-06-26 16:45:22 -0300

    Adapto Reglas y su Builder para usar Mediciones

--- src/main/java/dominio/Regla.java
+++ src/main/java/dominio/Regla.java
@@ -1,22 +1,22 @@
 package dominio;
 
-import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
 import dominio.actuadores.Actuador;
 
 public class Regla {
 
-	private BiFunction<Double, Magnitud, Boolean> condicion;
+	private Predicate<Medicion> condicion;
 	private Actuador actuador;
 	
-	public Regla(BiFunction<Double, Magnitud, Boolean> condicion, Actuador actuador) {
+	public Regla(Predicate<Medicion> condicion, Actuador actuador) {
 		super();
 		this.condicion = condicion;
 		this.actuador = actuador;
 	}
-
-	public void ejecutarSi(Double medicion, Magnitud magnitud) {
-		if(condicion.apply(medicion, magnitud))
-			this.actuador.actuar();
+	
+	public void ejecutarSi(Medicion medicion) {
+		if(medicion.comparar(condicion))
+			actuador.actuar();
 	}
 }
