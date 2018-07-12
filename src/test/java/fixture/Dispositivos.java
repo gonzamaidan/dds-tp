7268dab05076f1fafa8c1bc43986f8bc76f65238
iña <isauri.github2@goat.si>
2018-07-12 15:57:13 -0300

    Agrego metodos medio feos para usar facilmente Lamparas y AiresAcondicionados en los tests

--- src/test/java/fixture/Dispositivos.java
+++ src/test/java/fixture/Dispositivos.java
@@ -3,8 +3,11 @@ package fixture;
 import dominio.dispositivo.DispositivoEstandar;
 import dominio.dispositivo.DispositivoInteligente;
 import dominio.dispositivo.DispositivoInteligente.EstadoDispositivo;
+import dominio.dispositivo.fisicos.AireAcondicionado;
+import dominio.dispositivo.fisicos.DispositivoFisico;
 import dominio.dispositivo.fisicos.Lampara;
 import dominio.dispositivo.fisicos.TipoConcreto;
+import dominio.dispositivo.fisicos.TipoGenerico;
 
 public class Dispositivos {
 	
@@ -24,5 +27,12 @@ public class Dispositivos {
 	
 	public DispositivoInteligente dispositivoReal() {
 		return new DispositivoInteligente(new Lampara(TipoConcreto.De20W));
+	}
+	
+	public DispositivoFisico fisicoL(TipoConcreto tipo) {
+		return new Lampara(tipo);
+	}
+	public DispositivoFisico fisicoAC(TipoConcreto subTipo) {
+		return new AireAcondicionado(subTipo);
 	}
 }
